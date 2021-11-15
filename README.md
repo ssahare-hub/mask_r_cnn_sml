49a423cea8e24c566dcc5e1f39deaa11ef6134fd
Soham <sohamsahare@gmail.com>
2021-11-15 15:37:46 -0700

    Updated readme

--- README.md
+++ README.md
@@ -8,7 +8,8 @@ https://github.com/matterport/Mask_RCNN
 # steps
 - Download cityscapes dataset, specifically gtFine_trainvaltest and leftImg8bit_trainvaltest
 - extract gtFine and leftImg8bit to data/
-- run move_to_processed.py and you should get images in processed_data/
+- run move_to_processed.py and you should get images in processed_data/leftImg8bit
+- now toggle the commented code and run move_to_processed.py again and you should get json files in processed_data/gtFine
 - move /processed_data/gtFine/test/ to  processed_data/
 - run rename_files.py for each path ->  'gtFine/train' 'gtFine/val' 'leftImg8bit/train' 'leftImg8bit/val' 'leftImg8bit/test'
 - at the end of these steps, you should have .json files inside processed_data/gtFine/* and .png in processed_data/leftImg8bit/*
--- move_to_processed.py
+++ move_to_processed.py
@@ -1,21 +1,25 @@
 import os
 import re
 import shutil
+
+# for images
 SOURCE_DIR = './data/leftImg8bit'
-subpaths = os.listdir(SOURCE_DIR)
 OUT_DIR = './processed_data/leftImg8bit/'
 pattern = r'.+.png$'
 
+# for jsons
+# SOURCE_DIR = './data/gtFine'
+# OUT_DIR = './processed_data/gtFine/'
+# pattern = r'.+.json$'
+
+subpaths = os.listdir(SOURCE_DIR)
 for subpath in subpaths:
     # join the './gtFine' for walk dir
     sub_path = os.path.join(SOURCE_DIR, subpath)
-    
     # create the output dir
     if not os.path.exists(os.path.join(OUT_DIR, subpath)):
         os.mkdir(os.path.join(OUT_DIR, subpath))
-
     temp_destination = os.path.join(OUT_DIR, subpath)
-
     for dirpath, dirnames, filenames in os.walk(sub_path):
         for filename in filenames:
             if re.match(pattern, filename):
